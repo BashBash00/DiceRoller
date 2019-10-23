cec0d4304f571effecd146773fcb623989939991
BashBash00 <1820365@brunel.ac.uk>
2019-10-24 00:34:55 +0100

    commit, i dont know if it works because i cant test it, will check in lab tomorrow

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.diceroller;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int number_input;
+
+
+    EditText number_input1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,8 +65,31 @@ public class MainActivity extends AppCompatActivity {
     public void on_button_click(View view) {
 
         TextView tv = this.findViewById(R.id.textView);
+        TextView tv2 = this.findViewById(R.id.textView2);
+        TextView score = this.findViewById(R.id.Score);
+        int Score1 = 0;
+        Score1++;
+        score.setText(Integer.toString(Score1));
         Random r = new Random();
-        int number = r.nextInt(6);
+        int number = r.nextInt(7 - 1) + 1;
         tv.setText(Integer.toString(number));
+
+        number_input1 = findViewById(R.id.number_input);
+        number_input = Integer.valueOf(number_input1.getText().toString());
+        if (number_input == r.nextInt(7 - 1) + 1) {
+
+            tv2.setText("Congratulations !");
+            score.setText(Score1);
+            Score1++;
+
+
+
+        }
+        else{
+            tv2.setText("");
+        }
+
     }
 }
+
+
